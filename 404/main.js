f04354c48c8fad4a914614b34349aa23cf941160
Keir Clyne <KeirClyne@protonmail.com>
2021-01-25 18:42:09 +0000

    Changed: 404, Small Algo Changes

--- 404/main.js
+++ 404/main.js
@@ -1,14 +1,14 @@
-let y,z,speed,stepAmt,numWaves,waveStepAmt,numStreams,streamStepAmt,minHeight,heightSize;
+let y,z,speed,numVertex,numWaves,waveStepAmt,numStreams,streamStepAmt,minHeight,heightSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   y = 0;
   z = 0;
   speed = 0.003;
-  stepAmt = 20;
+  numVertex = 20;
   numWaves = 20;
   waveStepAmt = 0.02;
-  numStreams = 3;
+  numStreams = 5;
   streamsStepAmt = 5;
 }
 
@@ -18,7 +18,7 @@ function draw() {
 
   minHeight = height * 0.1;
   heightSize = height * 0.7;
-  let heightAdj = height * 0.15;
+  let heightAdj = height * 0.1;
   strokeWeight(0.5);
 
   for (let streams = 0; streams < numStreams; streams++) {
@@ -26,7 +26,7 @@ function draw() {
     for (let waves = 0; waves < numWaves; waves++) {
       stroke(s);
       beginShape();
-      for (let x = 0; x < width; x+=stepAmt) {
+      for (let x = 0; x < width; x+=numVertex) {
       let nx = (x * 0.001) + streams;
       let ny = z + (waves * waveStepAmt);
       y = (-noise(nx,ny) * heightSize) + height - (heightAdj * streams);
@@ -41,4 +41,17 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(window.innerWidth,window.innerHeight);
+}
+
+class Stream {
+  constructor() {
+    this.arr = [];
+    
+    let numWaves = 10;
+    for (let i = 0; i < numWaves; i++) {
+      this.arr.push(0);
+    }
+
+    
+  }
 }
