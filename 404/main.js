678121b36233249e9b5e452fd324ed904b1ba151
Keir Clyne <KeirClyne@protonmail.com>
2021-01-27 22:41:49 +0000

    CHANGED: 404, Waves smoother and adjusted to height of the screen

--- 404/main.js
+++ 404/main.js
@@ -1,40 +1,61 @@
 let y,z,speed,numVertex,numWaves,waveStepAmt,numStreams,streamStepAmt,minHeight,heightSize;
+let heightBetweenWaves,heightAdj,strokeArr,vertexFrameAdj,framerate;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   y = 0;
   z = 0;
-  speed = 0.003;
-  numVertex = 20;
+  speed = 0.001;
   numWaves = 20;
   waveStepAmt = 0.015;
-  numStreams = 5;
+  numStreams = 4;
   streamsStepAmt = 5;
+  vertexFrameAdj = 80;
+  
+  strokeArr = [];
+  for (let i = 0; i < numStreams; i++) {
+    strokeArr.push(map(i,0,numStreams,0,150));
+  }
+  
+  resetValues();
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth,window.innerHeight);
+  resetValues();
+}
+
+function resetValues() {
+  numVertex = Math.floor(width / vertexFrameAdj);
+  heightBetweenWaves = height * 0.003;
+  minHeight = height * 0.1;
+  heightSize = height * 0.9;
+  heightAdj = height * 0.05;
 }
 
 function draw() {
   background(255);
   noFill();
 
+  framerate = int(getFrameRate());
+  if (framerate < 40 && vertexFrameAdj > 10) {
+    vertexFrameAdj-=1;
+    numVertex = Math.floor(width / vertexFrameAdj);
+  }
+
   translate(0,height*0.2);
 
-  minHeight = height * 0.1;
-  heightSize = height * 0.7;
-  let heightAdj = height * 0.1;
   strokeWeight(0.5);
 
   for (let streams = 0; streams < numStreams; streams++) {
-    let s = map(streams,0,numStreams,0,150);
+    stroke(strokeArr[streams]);
     for (let waves = 0; waves < numWaves; waves++) {
-      stroke(s);
       beginShape();
-      for (let x = 0; x < width; x+=numVertex) {
-      let nx = (x * 0.001) + streams;
-      let ny = z + (waves * waveStepAmt);
-      y = (-noise(nx,ny) * heightSize) + height - (heightAdj * streams);
-      vertex(x,y);
+      for (let x = 0; x < width + (width*0.1); x+=numVertex) {
+        y = ((-(noise((x * 0.001) + streams,z + (waves * waveStepAmt) * 1.5) + (sin((frameCount*0.003) + (x*0.003) + (streams*0.7)) + 1) / 2) / 2 * heightSize) + height - (heightAdj * streams)) + (waves * heightBetweenWaves) * 2;
+        
+        vertex(x,y);
       }
-      vertex(width,y);
       endShape();
     }
   }
@@ -42,19 +63,13 @@ function draw() {
   z+=speed;
 }
 
-function windowResized() {
-  resizeCanvas(window.innerWidth,window.innerHeight);
-}
+/*
+        let nx = (x * 0.001) + streams;
+        let ny = z + (waves * waveStepAmt) * 1.5;
 
-class Stream {
-  constructor() {
-    this.arr = [];
-    
-    let numWaves = 10;
-    for (let i = 0; i < numWaves; i++) {
-      this.arr.push(0);
-    }
+        let s = (sin((frameCount*0.003) + (x*0.003) + (streams*0.7)) + 1) / 2;
+        let n = (noise(nx,ny) + s) / 2;
 
+        y = ((-n * heightSize) + height - (heightAdj * streams)) + (waves * heightBetweenWaves) * 2;
+*/
     
-  }
-}
