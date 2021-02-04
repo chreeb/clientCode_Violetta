d2686df2d97b7560fbe62937c67017847607190d
Keir Clyne <KeirClyne@protonmail.com>
2021-02-04 19:22:11 +0000

    CHANGED: Home, Changed to QUAD_STRIP to make faster

--- home/sketch.js
+++ home/sketch.js
@@ -7,12 +7,13 @@ function setup() {
   createCanvas(window.innerWidth,window.innerHeight,WEBGL);
 
   grid = [];
-  gridWidth = 30;
-  gridHeight = 30;
+  gridWidth = 40;
+  gridHeight = 40;
   for (let y = 0; y < gridHeight; y++) {
     let tempRow = [];
     for (let x = 0; x < gridWidth; x++) {
-      let tempVal = createVector(x,0,y);
+      let n = noise(x*0.2,y*0.2);
+      let tempVal = createVector(x,n,y);
       tempRow.push(tempVal);
     }
     grid.push(tempRow);
@@ -26,21 +27,25 @@ function windowResized() {
 function draw() {
   background(255);
 
-  rotateX(radians(57.4));
+  rotateX(radians(60));
   rotateZ(radians(45));
 
-  let adj = 20;
+  let mainSin = (sin(frameCount/20) + 1) / 2;
+
+  let adj = width / 50;
+  let heiAdj = height*0.2;
 
   for (let y = 0; y < gridHeight - 1; y++) {
+    beginShape(QUAD_STRIP);
     for (let x = 0; x < gridWidth - 1; x++) {
-
-      beginShape();
-      vertex(grid[x][y+1].x*adj,grid[x][y+1].z*adj,0);
-      vertex(grid[x][y].x*adj,grid[x][y].z*adj,0);
-      vertex(grid[x+1][y].x*adj,grid[x+1][y].z*adj,0);
-      vertex(grid[x+1][y+1].x*adj,grid[x+1][y+1].z*adj,0);
-      endShape();
+      vertex(grid[x][y+1].x*adj,   grid[x][y+1].z*adj,    (grid[x][y+1].y * mainSin)*heiAdj);
+      vertex(grid[x][y].x*adj,     grid[x][y].z*adj,      (grid[x][y].y * mainSin)*heiAdj);
+      vertex(grid[x+1][y].x*adj,   grid[x+1][y].z*adj,    (grid[x+1][y].y * mainSin)*heiAdj);
+      vertex(grid[x+1][y+1].x*adj, grid[x+1][y+1].z*adj,  (grid[x+1][y+1].y * mainSin)*heiAdj);
     }
+    endShape();
   }
 }
 
+// Fill quad strips
+// adjust camera (maybe using createGraphic and stretching to the size needed)
