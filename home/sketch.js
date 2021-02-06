a364cd60e79fc769a359cc1a171e479ef6c67213
Keir Clyne <KeirClyne@protonmail.com>
2021-02-06 21:10:23 +0000

    CHANED: Home, TriangleStrip grid

--- home/sketch.js
+++ home/sketch.js
@@ -1,76 +1,71 @@
 // chreebClient.home
 p5.disableFriendlyErrors = true;
 
-let grid,gridWidth,gridHeight,yChoice;
+let grid,gridWid,gridHei,numHeiChoices,currentHeiChoice;
 
 function setup() {
   createCanvas(window.innerWidth,window.innerHeight,WEBGL);
   smooth();
+  
+  gridWid = 30;
+  gridHei = 30;
+  numHeiChoices = 10;
+  currentHeiChoice = 0;
   grid = [];
-  gridWidth = 40;
-  gridHeight = 40;
-  for (let y = 0; y < gridHeight; y++) {
+
+  for (let y = 0; y < gridHei; y++) {
     let tempRow = [];
-    for (let x = 0; x < gridWidth; x++) {
-      let tempTile = new Tile(x,y);
+    for (let x = 0; x < gridWid; x++) {
+      let tempTile = {
+        x:x,
+        y:[],
+        z:y
+      }
+
+      for (let i = 0; i < numHeiChoices; i++) {
+        let n = noise(tempTile.x*0.1 + (i*10),tempTile.z*0.1 + (i*10));
+        n *= map(dist(x,y,gridWid,gridHei),0,gridWid,0,1);
+        tempTile.y.push(n);
+      }
+
       tempRow.push(tempTile);
-      /*
-      let n = noise(x*0.2,y*0.2);
-      let d = map(dist(x,y,0,0),0,40,1,0);
-      n = n*d;
-      let tempVal = createVector(x,n,y);
-      tempRow.push(tempVal);
-      */
     }
     grid.push(tempRow);
   }
-
-  yChoice = 0;
 }
- 
+
 function windowResized() {
   resizeCanvas(window.innerWidth,window.innerHeight);
 }
 
 function draw() {
-  background(255);
+  background(0,0,0,0);
+
+  let size = (width / height) * 50;
+
+  let heiMod = (Math.sin(frameCount/300) + 1) / 2;
+  if (heiMod < 0.0001) { currentHeiChoice++; }
+  if (currentHeiChoice >= numHeiChoices) { currentHeiChoice = 0; }
   
-  rotateX(radians(75));
+  rotateX(radians(70));
   rotateZ(radians(45));
-  translate(-50,-250,0);
+  translate(-(size * (gridWid-1))*0.6,-(size * (gridHei-1))*0.6,0);
+
   strokeWeight(0.7);
+  for (let y = 0; y < gridHei - 1; y++) {
+    let col = map(y,0,gridHei-1,200,255);
+    fill(col);
+    stroke(col-120);
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x < gridWid; x++) {
+      vertex(grid[x][y].x*size,
+             grid[x][y].z*size,
+             (grid[x][y].y[currentHeiChoice]*heiMod) * size*10);
 
-  let mainSin = (sin(frameCount/300) + 1) / 2;
-  if(mainSin < 0.0003) { yChoice++; }
-  if(yChoice > 50) { yChoice = 0; }
-
-  let adj = width / 100;//70;
-  let heiAdj = height*0.4;
-
-  for (let y = 0; y < gridHeight - 1; y++) {
-    beginShape(QUAD_STRIP);
-    for (let x = 0; x < gridWidth - 1; x++) {
-      vertex(grid[x][y+1].x*adj,   grid[x][y+1].z*adj,    (grid[x][y+1].y[yChoice] * mainSin)*heiAdj);
-      vertex(grid[x][y].x*adj,     grid[x][y].z*adj,      (grid[x][y].y[yChoice] * mainSin)*heiAdj);
-      vertex(grid[x+1][y].x*adj,   grid[x+1][y].z*adj,    (grid[x+1][y].y[yChoice] * mainSin)*heiAdj);
-      vertex(grid[x+1][y+1].x*adj, grid[x+1][y+1].z*adj,  (grid[x+1][y+1].y[yChoice] * mainSin)*heiAdj);
+      vertex(grid[x][y+1].x*size,
+             grid[x][y+1].z*size,
+             (grid[x][y+1].y[currentHeiChoice]*heiMod) * size*10);
     }
     endShape();
   }
 }
-
-class Tile {
-  constructor(x,z) {
-    this.x = x;
-    this.z = z;
-
-    this.y = [];
-    for (let i = 0; i < 50; i++) {
-      let n = noise(this.x*0.1 + (i*10),this.z*0.1 + (i*10)) * map(dist(this.x,this.z,0,0),0,40,1,0.1);
-      this.y.push(n);
-    }
-  }
-}
-
-// Fill quad strips
-// adjust camera (maybe using createGraphic and stretching to the size needed)
